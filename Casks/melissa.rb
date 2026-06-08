fbc5419ee57a624bc8b323109b3ce86d1c2e082e
TOMOKI O. <64746406+tomokio@users.noreply.github.com>
2026-06-08 23:14:44 +0900

    add cask melissa

---
+++ Casks/melissa.rb
@@ -0,0 +1,12 @@
+cask "melissa" do
+  version "4.0.2"
+  sha256 "3f7dc4aab53ee808da85f4d5db5a44b489e92c107ae71b22179c6b770f050374"
+
+  url "https://github.com/mosynthkey/Melissa/releases/download/v#{version}/Melissa_4_0_2.dmg"
+
+  name "Melissa"
+  desc "AI Audio Source Separation Feature-equipped music player for musical instrument practice"
+  homepage "https://mosynthkey.github.io/Melissa/"
+
+  pkg "Melissa_Installer_v#{version}.pkg"
+end
